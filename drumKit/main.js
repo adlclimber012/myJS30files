c2ca988fedbd3a974c6fd449f370f28c9f250b21
adlclimber012 <alexanderleblanc@Alexanders-MacBook-Pro.local>
2017-09-15 13:50:59 -0600

    JS portion
    In progress. Need to get the functions sorted out, but we have slight
    liftoff.

---
+++ drumKit/main.js
@@ -0,0 +1,16 @@
+var totalHits = 0;
+
+function playDrums() {
+    
+    
+    
+}
+
+document.getElementById("drum1").addEventListener("click", playDrums);
+document.getElementById("drum2").addEventListener("click", playDrums);
+document.getElementById("drum3").addEventListener("click", playDrums);
+document.getElementById("drum4").addEventListener("click", playDrums);
+document.getElementById("drum5").addEventListener("click", playDrums);
+document.getElementById("drum6").addEventListener("click", playDrums);
+document.getElementById("drum7").addEventListener("click", playDrums);
+document.getElementById("drum8").addEventListener("click", playDrums);
